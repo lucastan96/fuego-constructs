5d7e2955edc9cf74a04f0af374643408a916b077
Lucas Tan <lucastan96@gmail.com>
2019-02-08 19:11:37 +0000

    Added login screen

---
+++ app/src/main/java/com/fuego/mobile_ca1/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.fuego.mobile_ca1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button btnSignin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnSignin = findViewById(R.id.btn_login);
+        btnSignin.setOnClickListener(v -> signIn());
+    }
+
+    private void signIn() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
