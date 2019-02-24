ad4ba23cdb5c0e12bea4149037301fc21623a631
Sean Joyce <seanjoyce@outlook.ie>
2019-02-24 13:02:21 +0000

    Removed sites and type of user

--- app/src/main/java/com/fuego/mobile_ca1/Classes/User.java
+++ app/src/main/java/com/fuego/mobile_ca1/Classes/User.java
@@ -1,29 +1,23 @@
 package com.fuego.mobile_ca1.Classes;
 
-import com.fuego.mobile_ca1.enums.UserType;
-
-import java.util.Objects;
-
 public class User {
     private String uid;
     private String name;
     private int assignedSiteID;
     private String startTime;
     private String endTime;
-    private UserType typeOfUser;
     private Attendance attendance;
     private GeofenceTracker geofenceTracker;
 
     public User() {
     }
 
-    public User(String uid, String name, int assignedSiteID, String startTime, String endTime, UserType typeOfUser, Attendance attendance, GeofenceTracker geofenceTracker) {
+    public User(String uid, String name, int assignedSiteID, String startTime, String endTime, Attendance attendance, GeofenceTracker geofenceTracker) {
         this.uid = uid;
         this.name = name;
         this.assignedSiteID = assignedSiteID;
         this.startTime = startTime;
         this.endTime = endTime;
-        this.typeOfUser = typeOfUser;
         this.attendance = attendance;
         this.geofenceTracker = geofenceTracker;
     }
@@ -68,14 +62,6 @@ public class User {
         this.endTime = endTime;
     }
 
-    public UserType getTypeOfUser() {
-        return typeOfUser;
-    }
-
-    public void setTypeOfUser(UserType typeOfUser) {
-        this.typeOfUser = typeOfUser;
-    }
-
     public Attendance getAttendance() {
         return attendance;
     }
@@ -93,26 +79,6 @@ public class User {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        User user = (User) o;
-        return Objects.equals(getUid(), user.getUid()) &&
-                Objects.equals(getName(), user.getName()) &&
-                Objects.equals(getAssignedSite(), user.getAssignedSite()) &&
-                Objects.equals(getStartTime(), user.getStartTime()) &&
-                Objects.equals(getEndTime(), user.getEndTime()) &&
-                getTypeOfUser() == user.getTypeOfUser() &&
-                Objects.equals(getAttendance(), user.getAttendance()) &&
-                Objects.equals(getGeofenceTracker(), user.getGeofenceTracker());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getUid(), getName(), getAssignedSite(), getStartTime(), getEndTime(), getTypeOfUser(), getAttendance(), getGeofenceTracker());
-    }
-
-    @Override
     public String toString() {
         return "User{" +
                 "uid='" + uid + '\'' +
@@ -120,15 +86,9 @@ public class User {
                 ", assignedSite='" + assignedSiteID + '\'' +
                 ", startTime='" + startTime + '\'' +
                 ", endTime='" + endTime + '\'' +
-                ", typeOfUser=" + typeOfUser +
                 ", attendance=" + attendance +
                 ", geofenceTracker=" + geofenceTracker +
                 '}';
     }
 
 }
-
-
-
-
-
